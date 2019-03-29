b5cfe1ea792d78ff319f0543c647abe1c059cd96
Trevor Power <claydoughthings@gmail.com>
2019-03-29 23:08:14 +0000

    adding new object js code

--- objects.js
+++ objects.js
@@ -8,8 +8,60 @@ const address = {
     zipCode: 'E236HY'
 }
 
-function showAddress(obj) {
-    return console.log(obj);
+function showAddress(address) {
+    for (let key in address)
+        console.log(key, address[key]);
 }
 
 showAddress(address);
+
+// ____________________________________
+
+// Ex. 2 Factory and Constructor Functions
+
+let addressFactoryFunction = createAddress('a', 'b', 'c');
+let addressConstructor = new addressTwo('1', '2', '3');
+
+console.log(addressFactoryFunction);
+
+console.log(addressConstructor);
+
+// Factory
+function createAddress(street, city, zipCode) {
+    return {
+        street,
+        city,
+        zipCode
+    };
+}
+
+// Constructor (Pascal Notation)
+function addressTwo(street, city, zipCode) {
+    this.street = street;
+    this.city = city;
+    this.zipCode = zipCode;
+};
+
+// ____________________________________
+
+// Ex. 3 Object Equality
+
+let compAddress1 = new addressTwo('a', 'b', 'c');
+let compAddress2 = new addressTwo('a', 'b', 'c');
+
+function areEqual(compAddress1, compAddress2){
+    // are the values the same
+    return compAddress1.street === compAddress2.street &&
+        compAddress1.city === compAddress2.city &&
+        compAddress1.zipCode === compAddress2.zipCode;
+    // if same, return True
+}
+
+function areSame(compAddress1, compAddress2){
+    // are they at the same location 
+    // if not same location, return False
+    return compAddress1 === compAddress2;
+}
+
+console.log(areEqual(compAddress1, compAddress2));
+console.log(areSame(compAddress1, compAddress2));

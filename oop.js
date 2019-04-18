454f52199feed7e1c859fb9a8c01c1efd2873d8e
Trevor Power <claydoughthings@gmail.com>
2019-04-18 22:25:17 +0100

    oop, new playground

--- oop.js
+++ oop.js
@@ -1,3 +1,40 @@
 // oop with mosh
 
 console.log("Object Oriented Programming with JavaScript");
+
+function Stopwatch(){
+  //Private Methods
+  let startTime, endTime, running, duration = 0;
+
+  //Public Methods
+  this.start = function() {
+    if (running)
+    throw new Error("Already Started");
+    running = true;
+    startTime = new Date();
+  };
+
+  this.stop = function() {
+    if (!running)
+    throw new Error("Not Started");
+    running = false;
+    endTime = new Date();
+    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+    duration += seconds;
+  };
+
+  this.reset = function() {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  };
+
+let sw = new Stopwatch();
+
+sw.start();
+sw.stop();
+console.log(sw.duration);
+console.log("Fail");
+
+}
